ac515e5e6e4d1743ee7e9ac0177310a74e418b41
MilkyWay <thelovemadeus@gmail.com>
2024-05-31 20:51:38 +0200

    Slider captured continuously.

--- abnormality_labeling_app.m
+++ abnormality_labeling_app.m
@@ -7,15 +7,19 @@ function abnormality_labeling_app(fpath_to_video)
     ax.XTick = [];
     ax.YTick = [];
     
+    % Initialize the video reader
+    video = VideoReader(fpath_to_video);
+    
     % Create the slider for frame selection
     frameSlider = uislider(fig, 'Position', [20, 100, 640, 3], ...
-        'Limits', [1, 100000], 'Value', 1, 'ValueChangedFcn', @updateFrame);
+        'Limits', [1, video.NumFrames], 'Value', 1, 'ValueChangingFcn', @updateFrame);
     
     % Create the slider for frame rating (horizontal)
-   ratingSlider = uislider(fig, 'Position', [680, 400, 100, 3], ...
+    ratingSlider = uislider(fig, 'Position', [680, 400, 100, 3], ...
         'Limits', [0, 1], 'Value', 0, ...
         'MajorTicks', 0:0.2:1, ...
-        'MajorTickLabels', {'0', '0.2', '0.4', '0.6', '0.8', '1'});
+        'MajorTickLabels', {'0', '0.2', '0.4', '0.6', '0.8', '1'}, ...
+        'ValueChangingFcn', @updateRating);
     ratingLabel = uilabel(fig, 'Position', [680, 380, 100, 22], ...
         'Text', 'Level of abnormality');
     
@@ -34,9 +38,6 @@ function abnormality_labeling_app(fpath_to_video)
     % Create a label to display the current frame number
     frameLabel = uilabel(fig, 'Position', [680, 200, 100, 22], 'Text', 'Frame number: 1');
     
-    % Initialize the video reader
-    video = VideoReader(fpath_to_video);
-    
     % Initialize a data structure to store frame ratings
     ratings = zeros(video.NumFrames, 1);
     
@@ -44,12 +45,18 @@ function abnormality_labeling_app(fpath_to_video)
     isPlaying = false;
     
     % Update frame display
-    function updateFrame(~, ~)
-        frameNumber = round(frameSlider.Value);
+    function updateFrame(src, event)
+        frameNumber = round(event.Value);
         frameLabel.Text = ['Frame number: ', num2str(frameNumber)];
         video.CurrentTime = (frameNumber - 1) / video.FrameRate;
-        frame = readFrame(video);
+        frame = read(video, frameNumber);  % Read the specific frame
         imshow(frame, 'Parent', ax);
+    end
+    
+    % Update rating value
+    function updateRating(src, event)
+        frameNumber = round(frameSlider.Value);
+        ratings(frameNumber) = event.Value;
     end
     
     % Play video function
@@ -58,8 +65,12 @@ function abnormality_labeling_app(fpath_to_video)
         while hasFrame(video) && isPlaying
             frame = readFrame(video);
             imshow(frame, 'Parent', ax);
-            frameSlider.Value = video.CurrentTime * video.FrameRate;
-            frameNumber = round(frameSlider.Value);
+            frameNumber = round(video.CurrentTime * video.FrameRate);
+            if frameNumber >= video.NumFrames
+                frameNumber = video.NumFrames;
+                isPlaying = false;
+            end
+            frameSlider.Value = frameNumber;
             frameLabel.Text = ['Frame number: ', num2str(frameNumber)];
             ratings(frameNumber) = ratingSlider.Value;
             pause(1 / video.FrameRate);
